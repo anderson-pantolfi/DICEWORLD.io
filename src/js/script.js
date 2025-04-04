6b5d93bad3b2e5ace4510dd3b763566665e8ce36
anderson-pantolfi <andpantmoraes30@gmail.com>
2025-04-04 17:00:54 -0300

    estilização do menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,35 +1,28 @@
+"use strict";
 /* elements */
-
-const menuAboutGame = document.getElementById("aboutGame") as HTMLLIElement;
-const menuPersonagem = document.getElementById("personagem") as HTMLLIElement;
-const menuDesevolvimento = document.getElementById("desevolvimento") as HTMLLIElement;
-
-const SubMenuAboutGame = document.getElementById("subMenuAboutGame") as HTMLUListElement;
-const SubMenuPersonagem = document.getElementById("subMenuPersonagem") as HTMLUListElement;
-const subMenuDesevolvimento = document.getElementById("subMenuDesevolvimento") as HTMLUListElement;
-
+const menuAboutGame = document.getElementById("aboutGame");
+const menuPersonagem = document.getElementById("personagem");
+const menuDesevolvimento = document.getElementById("desevolvimento");
+const SubMenuAboutGame = document.getElementById("subMenuAboutGame");
+const SubMenuPersonagem = document.getElementById("subMenuPersonagem");
+const subMenuDesevolvimento = document.getElementById("subMenuDesevolvimento");
 /* events */
-
 menuAboutGame.addEventListener("mouseenter", () => {
     SubMenuAboutGame.style.display = "block";
 });
-
 menuAboutGame.addEventListener("mouseleave", () => {
     setTimeout(() => {
         if (!SubMenuAboutGame.matches(':hover')) {
             SubMenuAboutGame.style.display = "none";
         }
-    }, 300); 
+    }, 300);
 });
-
 SubMenuAboutGame.addEventListener("mouseleave", () => {
     SubMenuAboutGame.style.display = "none";
 });
-
 menuPersonagem.addEventListener("mouseenter", () => {
     SubMenuPersonagem.style.display = "block";
 });
-
 menuPersonagem.addEventListener("mouseleave", () => {
     setTimeout(() => {
         if (!SubMenuPersonagem.matches(':hover')) {
@@ -37,15 +30,12 @@ menuPersonagem.addEventListener("mouseleave", () => {
         }
     }, 300);
 });
-
 SubMenuPersonagem.addEventListener("mouseleave", () => {
     SubMenuPersonagem.style.display = "none";
 });
-
 menuDesevolvimento.addEventListener("mouseenter", () => {
     subMenuDesevolvimento.style.display = "block";
 });
-
 menuDesevolvimento.addEventListener("mouseleave", () => {
     setTimeout(() => {
         if (!subMenuDesevolvimento.matches(':hover')) {
@@ -53,7 +43,6 @@ menuDesevolvimento.addEventListener("mouseleave", () => {
         }
     }, 300);
 });
-
 subMenuDesevolvimento.addEventListener("mouseleave", () => {
     subMenuDesevolvimento.style.display = "none";
 });
